f6698cc011ed5348d7e0f7dee1c17b0a4cb3b554
Sandhiya003744 <sandhiyarameshkumar122@gmail.com>
2026-01-24 12:42:32 +0530

    AdminDashboard

---
+++ family-portal/routes/adminRoute.js
@@ -0,0 +1,7 @@
+
+const router = require("express").Router();
+const controller = require("../controllers/adminController");
+
+router.get("/dashboard", controller.dashboard);
+
+module.exports = router;
