67479efe7a108fec43057ff986e19e35bc1b2254
Nidhisaa <senthilnidhi57@gmail.com>
2026-01-31 11:49:34 +0530

    db changed

--- family-portal/config/db.js
+++ family-portal/config/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
-  password: "sandhiya@sowmiya2004",
+  password: "nidhi@20",
   database: "survey_app",
   // socketPath: '/tmp/mysql.sock',
   port:'3306'
