5b6d1c4b5f7bb8350e1e794475bb6f2c5c336465
sandy-192004 <sandhiyapalanivelr@gmail.com>
2026-02-05 15:26:24 +0530

    changed db

--- family-portal/config/db.js
+++ family-portal/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "sowmiya25",
+  password: "sandhiya@sowmiya2004",
   database: "survey_app",
   waitForConnections: true,
   connectionLimit: 10,
