383ff0c278270606845399b01cdcb4d2c69808bb
Sandhiya003744 <sandhiyarameshkumar122@gmail.com>
2026-02-14 12:42:41 +0530

    Admin dashboard UI

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const multer = require("multer");
+const multer = require("multer");
 const controller = require("../controllers/adminController");
 const familyController = require("../controllers/familyController"); // Newly added import for family controller
 
