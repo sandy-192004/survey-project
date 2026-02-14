a7a9d32aed2d55ce0c86f5060cf0d6449ca73006
Sandhiya003744 <sandhiyarameshkumar122@gmail.com>
2026-02-14 14:17:02 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
 
-  password: process.env.DB_PASSWORD || "sowmiya25",
-  database: process.env.DB_NAME || "survey_app",
+  password: process.env.DB_PASSWORD || "disneyTorn@123",
+  database: process.env.DB_NAME || "admin_db",
 
   waitForConnections: true,
   connectionLimit: 10,
